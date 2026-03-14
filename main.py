7d943fd0ca13064f665eeb73e3744e4203c06d1a
enzo <enzo.granana@ynov.com>
2026-03-14 16:54:27 +0100

    Add try match function

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from hash import GenerateHashs
+from match import TryMatch
 
 # Génération des hashs
-hashs = GenerateHashs("songs/California_Dreamin.mp3")
+hashsRecord = GenerateHashs("songs/California_Dreamin.mp3")
+
+# Test des matches sur chaque son
+# match = TryMatch()
